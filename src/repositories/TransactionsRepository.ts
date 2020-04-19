7f660be493e93419f7deb64fe910dfb865e1707a
Alan Rodrigues <alanalegrerodrigues@gmail.com>
2020-04-19 20:43:42 -0300

    Desafio finalizado

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,42 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const reducer = (acumulator: number, currentValue: Transaction): number =>
+      acumulator + currentValue.value;
+
+    const incomeTransctions = this.transactions.filter(
+      transaction => transaction.type === 'income',
+    );
+
+    const income = incomeTransctions.reduce(reducer, 0);
+
+    const outcomeTransctions = this.transactions.filter(
+      transaction => transaction.type === 'outcome',
+    );
+
+    const outcome = outcomeTransctions.reduce(reducer, 0);
+
+    const total = income - outcome;
+
+    const balance = {
+      income: income || 0,
+      outcome: outcome || 0,
+      total: total || 0,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
